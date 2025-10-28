93df93af8d9f19b20994e505051ad0fdc50f4390
Tanish Bhowmick <tanishbhowmick@gmail.com>
2025-10-27 21:37:09 -0400

    Assignmnet 7 prob 1

---
+++ Assignment_7/Prob1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Prob1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(91, 102);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(395, 20);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a YouTube URL: ";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(91, 137);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
+
---
+++ Assignment_7/Prob1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using YoutubeDLSharp;
+
+
+namespace Prob1
+{
+    public partial class Form1 : Form
+    {
+
+        private string binDir = AppContext.BaseDirectory; // yt-dlp.exe & ffmpeg.exe will later be saved here
+        private string targetDir = @"C:\transfer\YouTube";
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(binDir);
+            // download binaries
+            await YoutubeDLSharp.Utils.DownloadYtDlp(binDir);
+            await YoutubeDLSharp.Utils.DownloadFFmpeg(binDir);
+
+            //sanity check
+            if (!File.Exists(Path.Combine(binDir, "yt-dlp.exe")) ||
+            !File.Exists(Path.Combine(binDir, "ffmpeg.exe")))
+            {
+                MessageBox.Show("Failed to prepare yt-dlp or ffmpeg.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+        private async void btnDownload_Click(object sender, EventArgs e)
+        { 
+            
+            string url = txtUrl.Text.Trim();
+
+            // Check if directory is created. If not, create a new directory
+            if (!Directory.Exists(targetDir))
+            {
+                Directory.CreateDirectory(targetDir);
+            }
+
+            var ytdl = new YoutubeDL();
+            ytdl.YoutubeDLPath = Path.Combine(binDir, "yt-dlp.exe");
+            ytdl.FFmpegPath = Path.Combine(binDir, "ffmpeg.exe");
+            ytdl.OutputFolder = targetDir;
+            
+            // download video
+            var res = await ytdl.RunVideoDownload(url);
+            if (res.Success)
+            {
+                MessageBox.Show($"Success! Saved to:\n{res.Data}", "Download Complete",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Error:\n" + res.ErrorOutput, "Download Failed",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
